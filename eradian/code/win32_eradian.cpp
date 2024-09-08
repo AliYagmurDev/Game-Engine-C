42a5f838a0dbcbc99cac654e68727ab1d33af220
Ali Yağmur <ravenperception@gmail.com>
2024-09-08 22:15:59 +0300

    Trying to put colors on the screen but something is wrong yet

--- eradian/code/win32_eradian.cpp
+++ eradian/code/win32_eradian.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include <stdint.h>
 
 // define the static keyword with different names based on their usecase
 #define internal static
@@ -11,47 +12,61 @@ global_variable bool Running;
 // global variables for the bitmap to work
 global_variable BITMAPINFO BitmapInfo;
 global_variable void *BitmapMemory;
-global_variable HBITMAP BitmapHandle;
-global_variable HDC BitmapDeviceContext;
+global_variable int BitmapWidth;
+global_variable int BitmapHeight;
 
 // the function that runs every time the window is resized
 internal void Win32ResizeDIBSection(int Width, int Height) {
 
-    // free the bitmap memory if it exists
-    if(BitmapHandle) {
-        DeleteObject(BitmapHandle);
+    if(BitmapMemory) {
+        VirtualFree(BitmapMemory, 0, MEM_RELEASE);
     }
-    // create a new device context if it doesn't exist
-    if(!BitmapDeviceContext) {
-        BitmapDeviceContext = CreateCompatibleDC(0);
-    }
+
+    BitmapWidth = Width;
+    BitmapHeight = Height;
 
     // create a new bitmap info based on the width and height, later it will be used to copy our content into the window
     BITMAPINFO BitmapInfo;
     BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-    BitmapInfo.bmiHeader.biWidth = Width;
-    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+    BitmapInfo.bmiHeader.biHeight = BitmapHeight;
     BitmapInfo.bmiHeader.biPlanes = 1;
     BitmapInfo.bmiHeader.biBitCount = 32;
     BitmapInfo.bmiHeader.biCompression = BI_RGB;
 
-    // will work further on this, free first or free after
+    // allocate memory for the bitmap
+    int BitmapMemorySize = BitmapWidth * BitmapHeight * 4;
+    BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
 
-    BitmapHandle = CreateDIBSection(
-        BitmapDeviceContext,
-        &BitmapInfo,
-        DIB_RGB_COLORS,
-        &BitmapMemory,
-        0,
-        0
-    );
+    int Pitch = Width * 4;
+    UINT8 *Row = (UINT8 *)BitmapMemory;
+    for(int Y = 0; Y < BitmapHeight; Y++) {
+        UINT8 *Pixel = (UINT8 *)Row;
+        for(int X = 0; X < BitmapWidth; X++) {
+            *Pixel = 255;
+            ++Pixel;
+            *Pixel = 255;
+            ++Pixel;
+            *Pixel = 0;
+            ++Pixel;
+            *Pixel = 0;
+            ++Pixel;
+        }
+        Row += Pitch;
+    }
 }
 
-internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height) {
+internal void Win32UpdateWindow(HDC DeviceContext, RECT *WindowRect, int X, int Y, int Width, int Height) {
+    int WindowWidth = WindowRect->right - WindowRect->left;
+    int WindowHeight = WindowRect->bottom - WindowRect->top;
     StretchDIBits(
         DeviceContext,
+        /*
         X, Y, Width, Height,
         X, Y, Width, Height,
+        */
+        0, 0, BitmapWidth, BitmapHeight,
+        0, 0, WindowWidth, WindowHeight,
         BitmapMemory,
         &BitmapInfo,
         DIB_RGB_COLORS,
@@ -93,7 +108,11 @@ LRESULT CALLBACK Win32MainWindowCallback(
             int Y = Paint.rcPaint.top;
             int Width = Paint.rcPaint.right - Paint.rcPaint.left;
             int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-            Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+
+            Win32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
             EndPaint(Window, &Paint);
         } break;
 
