c04d8e2f3fde6eab736cd45232c93d718be4f927
AliYagmurGames <ravenperception@gmail.com>
2024-09-08 23:49:54 +0300

    Now we have some moving colurs on the screen, the code is still a little bit messy

--- eradian/code/win32_eradian.cpp
+++ eradian/code/win32_eradian.cpp
@@ -6,6 +6,8 @@
 #define local_persist static
 #define global_variable static
 
+typedef unsigned char uint8;
+
 // running controls the quit state of the application
 global_variable bool Running;
 
@@ -15,6 +17,21 @@ global_variable void *BitmapMemory;
 global_variable int BitmapWidth;
 global_variable int BitmapHeight;
 
+internal void RenderStuff(int XOffset, int YOffset) {  
+    int Pitch = BitmapWidth * 4;
+    UINT8 *Row = (UINT8 *)BitmapMemory;
+    for(int Y = 0; Y < BitmapHeight; Y++) {
+        UINT32 *Pixel = (UINT32 *)Row;
+        for(int X = 0; X < BitmapWidth; X++) {
+            UINT8 Blue = (X + XOffset);
+            UINT8 Green = (Y + YOffset);
+            UINT8 Red = 0;
+            UINT8 Alpha = 0;
+            *Pixel++ = ((Red << 16) | (Green << 8) | Blue | (Alpha << 24));
+        }
+        Row += Pitch;
+    }
+}
 // the function that runs every time the window is resized
 internal void Win32ResizeDIBSection(int Width, int Height) {
 
@@ -26,10 +43,9 @@ internal void Win32ResizeDIBSection(int Width, int Height) {
     BitmapHeight = Height;
 
     // create a new bitmap info based on the width and height, later it will be used to copy our content into the window
-    BITMAPINFO BitmapInfo;
     BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
     BitmapInfo.bmiHeader.biWidth = BitmapWidth;
-    BitmapInfo.bmiHeader.biHeight = BitmapHeight;
+    BitmapInfo.bmiHeader.biHeight = -BitmapHeight;
     BitmapInfo.bmiHeader.biPlanes = 1;
     BitmapInfo.bmiHeader.biBitCount = 32;
     BitmapInfo.bmiHeader.biCompression = BI_RGB;
@@ -38,33 +54,14 @@ internal void Win32ResizeDIBSection(int Width, int Height) {
     int BitmapMemorySize = BitmapWidth * BitmapHeight * 4;
     BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
 
-    int Pitch = Width * 4;
-    UINT8 *Row = (UINT8 *)BitmapMemory;
-    for(int Y = 0; Y < BitmapHeight; Y++) {
-        UINT8 *Pixel = (UINT8 *)Row;
-        for(int X = 0; X < BitmapWidth; X++) {
-            *Pixel = 255;
-            ++Pixel;
-            *Pixel = 255;
-            ++Pixel;
-            *Pixel = 0;
-            ++Pixel;
-            *Pixel = 0;
-            ++Pixel;
-        }
-        Row += Pitch;
-    }
+    // Should clear the screen to black maybe?
 }
 
-internal void Win32UpdateWindow(HDC DeviceContext, RECT *WindowRect, int X, int Y, int Width, int Height) {
-    int WindowWidth = WindowRect->right - WindowRect->left;
-    int WindowHeight = WindowRect->bottom - WindowRect->top;
+internal void Win32UpdateWindow(HDC DeviceContext, RECT *ClientRect, int X, int Y, int Width, int Height) {
+    int WindowWidth = ClientRect->right - ClientRect->left;
+    int WindowHeight = ClientRect->bottom - ClientRect->top;
     StretchDIBits(
         DeviceContext,
-        /*
-        X, Y, Width, Height,
-        X, Y, Width, Height,
-        */
         0, 0, BitmapWidth, BitmapHeight,
         0, 0, WindowWidth, WindowHeight,
         BitmapMemory,
@@ -139,7 +136,7 @@ int CALLBACK WinMain(
     WindowClass.lpszClassName = "EradianWindowClass";
     
     if(RegisterClassA(&WindowClass)) {
-        HWND WindowHandle = CreateWindowExA(
+        HWND Window = CreateWindowExA(
             0,
             WindowClass.lpszClassName,
             "Eradian",
@@ -153,18 +150,30 @@ int CALLBACK WinMain(
             Instance,
             0
         );
-        if(WindowHandle) {
+        if(Window) {
             Running = true;
+            int XOffset = 0;
+            int YOffset = 0;
             while(Running) {
+
                 MSG Message;
-                BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
-                if(MessageResult > 0) {
+                while(PeekMessageA(&Message, 0, 0, 0, PM_REMOVE)) {
+                    if(Message.message == WM_QUIT) {
+                        Running = false;
+                    }
                     TranslateMessage(&Message);
                     DispatchMessageA(&Message);
                 }
-                else {
-                    break;
-                }
+                RenderStuff(XOffset, YOffset);
+                HDC DeviceContext = GetDC(Window);
+                RECT ClientRect;
+                GetClientRect(Window, &ClientRect);
+                int WindowWidth = ClientRect.right - ClientRect.left;
+                int WindowHeight = ClientRect.bottom - ClientRect.top;
+
+                Win32UpdateWindow(DeviceContext, &ClientRect, 0, 0, WindowWidth, WindowHeight);
+                ReleaseDC(Window, DeviceContext);
+                ++XOffset;
             }
         }
         else {
