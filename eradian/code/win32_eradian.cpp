ac165740f5976946478f6a9fd336344c905b219f
AliYagmurGames <ravenperception@gmail.com>
2024-09-09 14:59:07 +0300

    A little bit clarifications and clean up. Everything is OK at this moment

--- eradian/code/win32_eradian.cpp
+++ eradian/code/win32_eradian.cpp
@@ -6,71 +6,101 @@
 #define local_persist static
 #define global_variable static
 
-typedef unsigned char uint8;
+// GlobalRunning controls the quit state of the application
+global_variable bool GlobalRunning;
 
-// running controls the quit state of the application
-global_variable bool Running;
+// define the offscreen buffer struct
+struct win32_offscreen_buffer {
+    // Pixels are always 32-bits wide, Memory Order BB GG RR XX
+    BITMAPINFO Info;
+    void *Memory;
+    int Width;
+    int Height;
+    int Pitch;
+};
 
-// global variables for the bitmap to work
-global_variable BITMAPINFO BitmapInfo;
-global_variable void *BitmapMemory;
-global_variable int BitmapWidth;
-global_variable int BitmapHeight;
+// define the global back buffer (It might not be here later)
+global_variable win32_offscreen_buffer GlobalBackBuffer;
 
-internal void RenderStuff(int XOffset, int YOffset) {  
-    int Pitch = BitmapWidth * 4;
-    UINT8 *Row = (UINT8 *)BitmapMemory;
-    for(int Y = 0; Y < BitmapHeight; Y++) {
+// define the window dimension struct for easy access
+struct win32_window_dimension {
+	int Width;
+	int Height;
+};
+
+// function for geting the window dimension
+internal win32_window_dimension Win32GetWindowDimension(HWND Window) {
+	win32_window_dimension Result;
+	RECT ClientRect;
+	GetClientRect(Window, &ClientRect);
+	Result.Width = ClientRect.right - ClientRect.left;
+	Result.Height = ClientRect.bottom - ClientRect.top;
+	return(Result);
+}
+
+// function for rendering stuff to the offscreen buffer
+// walkes each pixel size on memory and sets the color
+internal void RenderStuff(win32_offscreen_buffer Buffer, int XOffset, int YOffset) {      
+    UINT8 *Row = (UINT8 *)Buffer.Memory;
+    for(int Y = 0; Y < Buffer.Height; Y++) {
         UINT32 *Pixel = (UINT32 *)Row;
-        for(int X = 0; X < BitmapWidth; X++) {
+        for(int X = 0; X < Buffer.Width; X++) {
             UINT8 Blue = (X + XOffset);
             UINT8 Green = (Y + YOffset);
             UINT8 Red = 0;
             UINT8 Alpha = 0;
             *Pixel++ = ((Red << 16) | (Green << 8) | Blue | (Alpha << 24));
         }
-        Row += Pitch;
+        Row += Buffer.Pitch;
     }
 }
 // the function that runs every time the window is resized
-internal void Win32ResizeDIBSection(int Width, int Height) {
+internal void Win32ResizeDIBSection(win32_offscreen_buffer *Buffer, int Width, int Height) {
 
-    if(BitmapMemory) {
-        VirtualFree(BitmapMemory, 0, MEM_RELEASE);
+    Buffer->Width = Width;
+    Buffer->Height = Height;
+    // free the memory if it is already allocated
+    if(Buffer->Memory) {
+        VirtualFree(Buffer->Memory, 0, MEM_RELEASE);
     }
 
-    BitmapWidth = Width;
-    BitmapHeight = Height;
-
     // create a new bitmap info based on the width and height, later it will be used to copy our content into the window
-    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-    BitmapInfo.bmiHeader.biWidth = BitmapWidth;
-    BitmapInfo.bmiHeader.biHeight = -BitmapHeight;
-    BitmapInfo.bmiHeader.biPlanes = 1;
-    BitmapInfo.bmiHeader.biBitCount = 32;
-    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+    // -Height is for the top-down bitmap
+    Buffer->Info.bmiHeader.biSize = sizeof(Buffer->Info.bmiHeader);
+    Buffer->Info.bmiHeader.biWidth = Buffer->Width;
+    Buffer->Info.bmiHeader.biHeight = -Buffer->Height;
+    Buffer->Info.bmiHeader.biPlanes = 1;
+    Buffer->Info.bmiHeader.biBitCount = 32;
+    Buffer->Info.bmiHeader.biCompression = BI_RGB;
 
-    // allocate memory for the bitmap
-    int BitmapMemorySize = BitmapWidth * BitmapHeight * 4;
-    BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+    // allocate memory for to the buffer
+    int BitmapMemorySize = Buffer->Width * Buffer->Height * 4;
+    Buffer->Memory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+
+    Buffer->Pitch = Buffer->Width * 4;
 
     // Should clear the screen to black maybe?
 }
 
-internal void Win32UpdateWindow(HDC DeviceContext, RECT *ClientRect, int X, int Y, int Width, int Height) {
-    int WindowWidth = ClientRect->right - ClientRect->left;
-    int WindowHeight = ClientRect->bottom - ClientRect->top;
+// function for copying the buffer to the window
+internal void Win32CopyBufferToWindow(  HDC DeviceContext, int WindowWidth, int WindowHeight,
+                                        win32_offscreen_buffer Buffer) 
+{
     StretchDIBits(
         DeviceContext,
-        0, 0, BitmapWidth, BitmapHeight,
         0, 0, WindowWidth, WindowHeight,
-        BitmapMemory,
-        &BitmapInfo,
+        0, 0, Buffer.Width, Buffer.Height,
+        Buffer.Memory,
+        &Buffer.Info,
         DIB_RGB_COLORS,
         SRCCOPY
     );
 }
 
+// the main window callback function
+// runs for every message that the window receives
+// each case interrupts the message and initate our logic
+// all other cases handled by the default windows procedure
 LRESULT CALLBACK Win32MainWindowCallback(
     HWND Window,
     UINT Message,
@@ -81,24 +111,22 @@ LRESULT CALLBACK Win32MainWindowCallback(
 
     switch(Message) {
         case WM_SIZE: {
-            RECT ClientRect;
-            GetClientRect(Window, &ClientRect);
-            int Width = ClientRect.right - ClientRect.left;
-            int Height = ClientRect.bottom - ClientRect.top;
-            Win32ResizeDIBSection(Width, Height);
+            // when the window is resized
+
         } break;
         case WM_DESTROY: {
             // when the window is destroyed
-            Running = false;
+            GlobalRunning = false;
         } break;
         case WM_CLOSE: {
             // when user tries to quit by clicking the X button
-            Running = false;
+            GlobalRunning = false;
         } break;
         case WM_ACTIVATEAPP: {
             OutputDebugStringA("WM_ACTIVATEAPP\n");
         } break;
         case WM_PAINT: {
+            // when the window needs to be repainted
             PAINTSTRUCT Paint;
             HDC DeviceContext = BeginPaint(Window, &Paint);
             int X = Paint.rcPaint.left;
@@ -106,15 +134,13 @@ LRESULT CALLBACK Win32MainWindowCallback(
             int Width = Paint.rcPaint.right - Paint.rcPaint.left;
             int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
 
-            RECT ClientRect;
-            GetClientRect(Window, &ClientRect);
-
-            Win32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
+            win32_window_dimension Dimension = Win32GetWindowDimension(Window);
+            Win32CopyBufferToWindow(DeviceContext, Dimension.Width, Dimension.Height, GlobalBackBuffer);
             EndPaint(Window, &Paint);
         } break;
 
         default: {
-            //OutputDebugStringA("default\n");
+            // default windows procedure for everything else
             Result = DefWindowProc(Window, Message, WParam, LParam);
         } break;
     }
@@ -122,19 +148,31 @@ LRESULT CALLBACK Win32MainWindowCallback(
     return(Result);
 }
 
+// the main function where windows starts executing the code
 int CALLBACK WinMain(
     HINSTANCE Instance,
     HINSTANCE PrevInstance,
     LPSTR CommandLine,
     int ShowCode) 
 {
+    // create a window class for communication with the window
     WNDCLASSA WindowClass = {};
-    //WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+
+    //win32_window_dimension Dimension = Win32GetWindowDimension(Window);
+    Win32ResizeDIBSection(&GlobalBackBuffer, 1280, 720);
+
+    // fill the window class with the necessary information
+    // current style is for redrawing the whole window when it is resized
+    WindowClass.style = CS_HREDRAW | CS_VREDRAW;
+    // the callback function for the window to manage the messages
     WindowClass.lpfnWndProc = Win32MainWindowCallback;
+    // the instance of the application
     WindowClass.hInstance = Instance;
-    // WindowClass.hIcon = ;
+    // WindowClass.hIcon = ; // for defining an icon
+    // Below is the name for the window class
     WindowClass.lpszClassName = "EradianWindowClass";
     
+    // register the window class
     if(RegisterClassA(&WindowClass)) {
         HWND Window = CreateWindowExA(
             0,
@@ -150,29 +188,37 @@ int CALLBACK WinMain(
             Instance,
             0
         );
+        // if the window is created successfully
         if(Window) {
-            Running = true;
+            GlobalRunning = true;
             int XOffset = 0;
             int YOffset = 0;
-            while(Running) {
+            // the main loop for the application
+            while(GlobalRunning) {
 
                 MSG Message;
+                // run for all the messages in the message queue and dispatch them
+                // exit when no message is left in the queue
                 while(PeekMessageA(&Message, 0, 0, 0, PM_REMOVE)) {
                     if(Message.message == WM_QUIT) {
-                        Running = false;
+                        GlobalRunning = false;
                     }
+                    //Turns messages into something useful
                     TranslateMessage(&Message);
+                    //Sends the message to the callback function
                     DispatchMessageA(&Message);
                 }
-                RenderStuff(XOffset, YOffset);
-                HDC DeviceContext = GetDC(Window);
-                RECT ClientRect;
-                GetClientRect(Window, &ClientRect);
-                int WindowWidth = ClientRect.right - ClientRect.left;
-                int WindowHeight = ClientRect.bottom - ClientRect.top;
+                // render some stuff to the offscreen buffer
+                // this is where the fun resides right now
+                RenderStuff(GlobalBackBuffer, XOffset, YOffset);
 
-                Win32UpdateWindow(DeviceContext, &ClientRect, 0, 0, WindowWidth, WindowHeight);
-                ReleaseDC(Window, DeviceContext);
+                // copy the buffer to the window
+                HDC DeviceContext = GetDC(Window); // get the device context for the window
+                win32_window_dimension Dimension = Win32GetWindowDimension(Window);
+                Win32CopyBufferToWindow(DeviceContext, Dimension.Width, Dimension.Height, GlobalBackBuffer);
+                ReleaseDC(Window, DeviceContext); // release the device context (must happen each time you get it)
+
+                // part of the current fun, wont be here later
                 ++XOffset;
             }
         }
